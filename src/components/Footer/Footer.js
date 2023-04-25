ae9a0a7f29c331f2bfd02f2ed7946123c70fe9ec
anmoljain16 <75519593+anmoljain16@users.noreply.github.com>
2023-04-25 23:28:11 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p className="copyright">
+        Copyright &copy; 2023{" "}
+        <span>
+          Powered by: <b className="food">Anmol Jain</b>
+        </span>
+      </p> 3
+    </footer>
+  );
+}
+
+export default Footer;
+
